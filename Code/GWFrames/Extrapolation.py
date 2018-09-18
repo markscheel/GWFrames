c92244a31bb236e73c7c1a82c70317db1247d735
Mark Scheel <scheel@tapir.caltech.edu>
2018-09-18 13:40:17 -0700

    Automatically drop outermost extraction radius if not valid.

--- Code/GWFrames/Extrapolation.py
+++ Code/GWFrames/Extrapolation.py
@@ -181,6 +181,11 @@ def ReadFiniteRadiusData(ChMass=0.0, filename='rh_FiniteRadii_CodeUnits.h5', Coo
         if(not CoordRadii) :
             # If the list of Radii is empty, figure out what they are
             CoordRadii = [m.group('r') for Name in WaveformNames for m in [re_compile(r"""R(?P<r>.*?)\.dir""").search(Name)] if m]
+            if(not ValidateGroupOfWaveforms(f,filename,WaveformNames,LModes)) :
+                print("Invalid waveform. Trying without outermost extr. radius")
+                print("Coord radii={},WaveformNames={}".format(CoordRadii,WaveformNames))
+                CoordRadii.pop()
+                WaveformNames.pop()
         else :
             # Pare down the WaveformNames list appropriately
             if(type(CoordRadii[0])==int) : CoordRadii = [WaveformNames[i] for i in CoordRadii]
